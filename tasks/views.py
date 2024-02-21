0c19945bf011be32d66a820266bc2406d405261a
tRaihanSarkar <t.raihansarkar@gmail.com>
2024-02-22 01:28:59 +0600

    Added details view of tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
+from django.urls import reverse
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
 from .serializers import TaskSerializer
 
-from .models import Task
+from .models import Image, Task
 
 # Create your views here.
 def index(request):
@@ -19,14 +20,33 @@ def index(request):
 
 def create(request):
     if request.method == "POST":
-        title = request.title
-        description = request.description
-        date = request.date
+        title = request.POST['title']
+        description = request.POST['description']
+        date = request.POST['date']
+        priority = request.POST['priority']
+        uploaded_images = request.FILES.getlist('image')  # Replace with your actual field name
+        task = Task(title=title,description=description,due_date=date,priority=priority)
+        task.save()
+        for image in uploaded_images:
+            # Handle upload of images
+            image_obj = Image(task=task,images=image)
+            image_obj.save()
+            
+        return HttpResponseRedirect(reverse("work:index"),{
+            'success': "Task Added Sucessfully",
+        })
         
     return render(request, "tasks/create.html")
 
-def detail(request):
-    pass
+
+
+def detail(request, id):
+    task = get_object_or_404(Task, pk=id)
+    images = Image.objects.filter(task=task)
+    return render(request, "tasks/detail.html",{
+        'task': task,
+        'images': images
+    })
 
 # Rest API
     
