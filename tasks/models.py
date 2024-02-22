8ae7384fdad6cba5a5e5a524e2b26b442707af54
tRaihanSarkar <t.raihansarkar@gmail.com>
2024-02-22 15:07:35 +0600

    Changed models.py

--- tasks/models.py
+++ tasks/models.py
@@ -9,7 +9,6 @@ class Task(models.Model):
     }
     title = models.CharField(max_length=250)
     description = models.TextField()
-    photos = models.FileField(blank=True)
     created_at = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField(auto_now=False)
     priority = models.CharField(max_length=6, choices=priority_choices, default='LOW')
